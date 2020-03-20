08f6a9e600cec33d0602cd35ea1e5d7f652d2bb4
Matheus Pegoraro <matheuspegoraro@icloud.com>
2020-03-20 16:15:30 -0300

    Adicionando web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -12,6 +13,7 @@ mongoose.connect('mongodb+srv://matheus:matheus@omnistack10-kyecw.mongodb.net/te
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
---
+++ web/src/App.js
@@ -0,0 +1,132 @@
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+import DevItem from './components/DevItem';
+
+// Três pilares do React
+// ** Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+// ** Propriedade: Informações que um componente PAI passa para o componente FILHO
+// ** Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
+
+function App() {
+  const [devs, setDevs] = useState([]);
+
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      error => {
+        console.error(error);
+      }, {
+        timeout: 30000
+      }
+    )
+  }, []);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    });
+
+    setGithubUsername('');
+    setTechs('');
+
+    setDevs([...devs, response.data]);
+  }
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form onSubmit={handleAddDev}>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input 
+              name="github_username" 
+              id="github_username" 
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+              required 
+            />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input 
+              name="techs" 
+              id="techs"
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+              required 
+            />
+          </div>
+          
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input 
+                type="number" 
+                name="latitude" 
+                id="latitude" 
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)} 
+                required  
+              />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input 
+                type="number" 
+                name="longitude" 
+                id="longitude" 
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)} 
+                required  
+              />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
